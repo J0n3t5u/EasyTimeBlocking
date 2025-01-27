d1a6f6717d4fb5e9d39bb8c2346d15e08cbe6b2d
Alex <Al3x.3.m4r14n@gmail.com>
2025-01-26 23:10:25 -0500

    Initial commit, rough outline for the function that's going to clean up any input text for a given schedule

---
+++ UndescriptFunctions.ts
@@ -0,0 +1,51 @@
+const weekdays = [
+    "sunday", 
+    "monday",
+    "tuesday",  //Continuity might need to be checked to seperate tuesday from thursday if it goes by single character formatting. (SMTWTFS)
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday"
+]
+
+//debug
+let bogusSchedule = `Sun 1/26 12:00PM to 9:45PM (2 Shifts) 
+
+#299408 - 894 NowhereVille Av Unit 3421, Somewhere, NY
+
+Mon 1/27 2:30PM to 10:15PM
+
+#299408 - 894 NowhereVille Av Unit 3421, Somewhere, NY
+
+Tue 1/28 3:00PM to 10:15PM
+
+#299408 - 894 NowhereVille Av Unit 3421, Somewhere, NY
+
+Thu 2/6 12:00PM to 10:00PM (2 Shifts)
+
+#299408 - 894 NowhereVille Av Unit 3421, Somewhere, NY
+
+Fri 2/7 5:00PM to 10:30PM
+
+#299408 - 894 NowhereVille Av Unit 3421, Somewhere, NY
+
+Sat 2/8 3:00PM to 10:30PM
+
+#299408 - 894 NowhereVille Av Unit 3421, Somewhere, NY
+
+Sun 2/9 12:00PM to 9:45PM (2 Shifts)
+
+#299408 - 894 NowhereVille Av Unit 3421, Somewhere, NY
+
+Mon 2/10 2:30PM to 10:00PM
+
+#299408 - 894 NowhereVille Av Unit 3421, Somewhere, NY`
+
+
+let processStringToData = function (input:String,seperator:String) {
+    for (const i in input.split('')) { //Kind of gross.
+        console.log(i);
+    }   
+}
+
+processStringToData(bogusSchedule, " ");
